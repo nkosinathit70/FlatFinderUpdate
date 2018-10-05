1eb345f8c35d2232ddc5757bdfbacc763e22a4c7
nkosinathi terrence <nkosinathit70@gmail.com>
2018-10-05 16:00:23 -0700

    first commit

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -49,9 +49,9 @@ this.role = this.navParams.get("role");
 console.log("role",this.role);
     this.login = this.fb.group({
      
-      email:['',Validators.compose([Validators.maxLength(25),Validators.pattern('^[a-zA-Z0-9@]'),Validators.required])],
-      password:['',Validators.compose([Validators.minLength(8),Validators.pattern('^[a-zA-Z0-9!@#$%^&*]'),Validators.required])]
-      
+      email: ['', Validators.compose([Validators.maxLength(70), Validators.pattern('^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$'), Validators.required])],
+     //password:['',Validators.compose([Validators.minLength(8),Validators.pattern('^[a-zA-Z0-9!@#$%^&*]'),Validators.required])]
+     password:['',[Validators.required,Validators.minLength(6)]],
     });
 
 
